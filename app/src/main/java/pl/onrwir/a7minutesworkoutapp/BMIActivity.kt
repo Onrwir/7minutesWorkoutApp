9f200706449fda231789b44520a893c66fcbc6bc
Onrwir <115116487+Onrwir@users.noreply.github.com>
2024-04-16 23:12:22 +0200

    Created BMI calculator layout elements and implemented functionality

--- app/src/main/java/pl/onrwir/a7minutesworkoutapp/BMIActivity.kt
+++ app/src/main/java/pl/onrwir/a7minutesworkoutapp/BMIActivity.kt
@@ -1,12 +1,16 @@
 package pl.onrwir.a7minutesworkoutapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import pl.onrwir.a7minutesworkoutapp.databinding.ActivityBmiBinding
 import pl.onrwir.a7minutesworkoutapp.databinding.ActivityMainBinding
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class BMIActivity : AppCompatActivity() {
     private var binding: ActivityBmiBinding? = null
@@ -23,10 +27,73 @@ class BMIActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
             supportActionBar?.title = "CALCULATE BMI"
         }
+
         binding?.toolbarBMIActivity?.setNavigationOnClickListener{
             onBackPressed()
+        }
 
+        binding?.btnCalculateUnits?.setOnClickListener {
+            if (validateMetricUnit()){
+                val heightValue : Float = binding?.etMetricUnitHeight?.text.toString().toFloat() / 100
+                val weightValue : Float = binding?.etMetricUnitWeight?.text.toString().toFloat()
+
+                val bmi = weightValue / (heightValue*heightValue)
+
+                displayBMIResult(bmi)
+
+            }else{
+                Toast.makeText(this@BMIActivity,"Please enter valid values.",Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
+
+    private fun displayBMIResult(bmi:Float){
+        val bmiLabel : String
+        val bmiDescription : String
+
+        if(bmi.compareTo(15f) <= 0){
+            bmiLabel = "Very severely underweight"
+            bmiDescription = "Opps! You really need to take better care of yourself! Eat more!"
+        }else if (bmi.compareTo(16f) > 0 && bmi.compareTo(18.5f) <= 0){
+            bmiLabel = "Underweight"
+            bmiDescription = "Opps! You really need to take better care of yourself! Eat more!"
+        }else if (bmi.compareTo(18.5f) > 0 && bmi.compareTo(25f) <= 0) {
+            bmiLabel = "Normal"
+            bmiDescription = "Congratulations! You are in a good shape!"
+        }else if (bmi.compareTo(25f) > 0 && bmi.compareTo(30f) <= 0) {
+            bmiLabel = "Overweight"
+            bmiDescription = "Opps! You really need to take better care of yourself! Workout more"
+        }else if (bmi.compareTo(30f) > 0 && bmi.compareTo(35f) <= 0) {
+            bmiLabel = "Obese Class | (Moderately obese)"
+            bmiDescription = "Opps! You really need to take better care of yourself! Workout more"
+        }else if (bmi.compareTo(35f) > 0 && bmi.compareTo(40f) <= 0) {
+            bmiLabel = "Obese Class || (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }else{
+            bmiLabel = "Obese Class ||| (Severely obese)"
+            bmiDescription = "OMG! You are in a very dangerous condition! Act now!"
+        }
+
+        val bmiValue = BigDecimal(bmi.toDouble())
+            .setScale(2, RoundingMode.HALF_EVEN).toString()
+
+        binding?.llDisplayBMIResult?.visibility = View.VISIBLE
+        binding?.tvBMIValue?.text = bmiValue
+        binding?.tvBMIType?.text = bmiLabel
+        binding?.tvBMIDescription?.text = bmiDescription
+    }
+
+    private fun validateMetricUnit():Boolean{
+        var isValid = true
+
+        if (binding?.etMetricUnitWeight?.text.toString().isEmpty()){
+            isValid= false
+        }else if (binding?.etMetricUnitHeight?.text.toString().isEmpty()){
+            isValid= false
+        }
+        return isValid
+    }
+
+
 }
